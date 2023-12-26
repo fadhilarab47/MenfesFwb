023bfdedc89f507a64dec25d97b39a09484edfcb
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-26 16:46:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os
 
 api_id = int(os.environ.get("API_ID", "15890589"))
 api_hash = os.environ.get("API_HASH", "27fe60ebafe8a74117bfae10407925c7")
-bot_token = os.environ.get("BOT_TOKEN", "6518034019:AAF8SmArIfc0DaKfw_KC0uiG0JvoOsefoHc")
+bot_token = os.environ.get("BOT_TOKEN", "6826195273:AAFjSeGQGm87MdjGxBGFBbbkCoBm3STIGtg")
 # =========================================================== #
 
-db_url = os.environ.get("DB_URL", "mongodb+srv://Arab01:fadhil123@cluster0.ul5qaif.mongodb.net/")
-db_name = os.environ.get("DB_NAME", "Arab01")
+db_url = os.environ.get("DB_URL", "mongodb+srv://doadmin:9r260Iqy437zS1lA@db-mongodb-sgp1-52558-1312a8db.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-sgp1-52558")
+db_name = os.environ.get("DB_NAME", "doadmin")
 # =========================================================== #
 
 channel_1 = int(os.environ.get("CHANNEL_1", "-1002090852874"))
-channel_2 = int(os.environ.get("CHANNEL_2", "-1002132564390"))
-channel_log = int(os.environ.get("CHANNEL_LOG", "-1002070266863"))
+channel_2 = int(os.environ.get("CHANNEL_2", "-1001665881786"))
+channel_log = int(os.environ.get("CHANNEL_LOG", "-1002130415408"))
 # =========================================================== #
 
-id_admin = int(os.environ.get("ID_ADMIN", "1506027871"))
+id_admin = int(os.environ.get("ID_ADMIN", "1948147616"))
 # =========================================================== #
 
 batas_kirim = int(os.environ.get("BATAS_KIRIM", "3"))
@@ -35,27 +35,28 @@ biaya_gfrent = int(os.environ.get("BIAYA_GFRENT", "200"))
 biaya_bfrent = int(os.environ.get("BIAYA_BFRENT", "200"))
 # =========================================================== #
 
-hastag = os.environ.get("HASTAG", "#MikuGirl #MikuBoy #MikuAsk #MikuStory #MikuSpill #MikuFind").replace(" ", "|").lower()
+hastag = os.environ.get("HASTAG", "#PixyGirl #PixyBoy #PixyCurhat #PixySpill #PixyPap").replace(" ", "|").lower()
 # =========================================================== #
 
-pic_boy = os.environ.get("PIC_BOY", "https://telegra.ph//file/a6896b2fb8d013b7832e3.jpg")
-pic_girl = os.environ.get("PIC_GIRL", "https://telegra.ph//file/a3ad21e5abdeb32deddcf.jpg")
-pic_talent = os.environ.get("PIC_TALENT", "https://telegra.ph//file/31aa5b2804691ec04ed9c.jpg")
-pic_bf_rent = os.environ.get("PIC_BF_RENT", "https://telegra.ph//file/8de768f664063f1bb520e.jpg")
-pic_gf_rent = os.environ.get("PIC_GF_RENT", "https://telegra.ph//file/983e3138a5883645497d3.jpg")
-pic_sugar_daddy = os.environ.get("PIC_SUGAR_DADDY", "https://telegra.ph//file/df87a56dfa94043c51572.jpg")
+pic_boy = os.environ.get("PIC_BOY", "https://telegra.ph//file/b1b084fdcc3254aa4c212.jpg")
+pic_girl = os.environ.get("PIC_GIRL", "https://telegra.ph//file/3db21c15c059d6a6080d1.jpg")
+pic_talent = os.environ.get("PIC_TALENT", "")
+pic_bf_rent = os.environ.get("PIC_BF_RENT", "")
+pic_gf_rent = os.environ.get("PIC_GF_RENT", "")
+pic_sugar_daddy = os.environ.get("PIC_SUGAR_DADDY", "")
 # =========================================================== #
 
-pesan_join = os.environ.get("PESAN_JOIN", "Hai {mention} Sobat Miku😉\n\nKamu Tidak dapat Mengirim Menfes , Harap Join Terllebih Dahulu Untuk Mengirim Menfess ya Miku👍")
-start_msg = os.environ.get("START_MSG", "Hai {fullname} hallo selamat datang dibot auto post Miku base✨\n\nIni adalah bot Menfes ya FWB, semua pesan yang kamu kirim akan masuk ke channel secara anonim sesuai hastag:\n#MikuGirl Untuk Cewek\n#MikuBoy Untuk Cowok\n#MikuAsk Untuk Bertanya\n#MikuStory Untuk Cerita\n#MikuSpill Untuk Spill\n#MikuFind untuk Mencari Pasangan\n\nContoh:\n{mention} Cari Mutualan Dom Depok #MikuGirl\n\nContact @Mikuchannn")
+pesan_join = os.environ.get("PESAN_JOIN", "Hai {mention} Sobat Pixy Base [😉](https://telegra.ph//file/b52849db161f0e56e3461.jpg) ,\n\nKamu Tidak dapat Mengirim Menfes , Harap Join Terllebih Dahulu Untuk Mengirim Menfess di Pixy Base👍")
+start_msg = os.environ.get("START_MSG", "Hai {fullname} hallo selamat datang dibot auto post Pixy Menfes base [✨](https://telegra.ph//file/b52849db161f0e56e3461.jpg) .\n\nIni adalah bot Menfes ya BG Menfes, semua pesan yang kamu kirim akan masuk ke channel secara anonim sesuai hastag:\n#PixyGirl Untuk Cewek\n#PixyBoy Untuk Cowok\n#PixyCurhat Untuk Curhat\n#PixyPap Untuk Mengirim Pap random kalian\n\nContoh:\n. {mention} Cari Mutualan Dom Depok @fulann #PixyGirl\n\nSupport: @Pixy_Update")
 
 gagalkirim_msg = os.environ.get("GAGAL_KIRIM", """
-{mention}, pesan mu gagal terkirim silahkan gunakan hastag:
+Halo {mention} 
 
-#MikuBoy / #MikuGirl Untuk Mencari Pasangan, Teman , Partner FWB
-#MikuAsk Untuk Bertanya
-#MikuStory Untuk Berbagi Cerita
-#MikuSpill Untuk Spill Masalah
-#MikuFind Untuk Mencari Pasangan, Teman, Partner FWB.
+pesan mu gagal terkirim silahkan gunakan hastag:
+#PixyGirl / #PixyBoy Untuk Mencari Pasangan, Teman , Partner FWB
+#PixyAsk Untuk Bertanya
+#PixyCurhat Untuk Berbagi Cerita / Curhat.
+#PixySpill Untuk Spill Masalah / Pengalaman
+#PixyPap Untuk Mengirim Pap random kalian.
 
 """)
